d34b724c5ca5638495cc7a412adeab7f23f92ed5
Ruben Melikyan <melikyan740@gmail.com>
2025-11-30 11:58:27 -0800

    Module 5: Refactor API, add REST endpoints, Postman-tested

--- app_api/models/trips.js
+++ app_api/models/trips.js
@@ -1,37 +1,19 @@
+
 const mongoose = require('mongoose');
 
-const tripSchema = new mongoose.Schema({
-  code: {
-    type: String,
-    required: true,
-    unique: true
+const tripSchema = new mongoose.Schema(
+  {
+    code: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    length: { type: String, required: true },
+    start: { type: Date, required: true },
+    resort: { type: String, required: true },
+    perPerson: { type: Number, required: true, min: 0 },
+    image: { type: String },
+    description: { type: String, required: true }
   },
-  name: {
-    type: String,
-    required: true
-  },
-  length: {
-    type: String,
-    required: true
-  },
-  start: {
-    type: Date,
-    required: true
-  },
-  resort: {
-    type: String,
-    required: true
-  },
-  perPerson: {
-    type: Number,
-    required: true,
-    min: 0
-  },
-  image: String,
-  description: {
-    type: String,
-    required: true
-  }
-});
+  { collection: 'trips' } 
+);
 
 module.exports = mongoose.model('Trip', tripSchema);
+
